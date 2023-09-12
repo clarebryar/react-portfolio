ed65129d6f88d9c21ee1cef1f79168833755aa0d
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-09-12 00:01:42 -0500

    about me page, and contact page

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -4,8 +4,31 @@ import Footer from "../components/Footer";
 import Contact from "../components/Contact";
 import Project from "../components/Project";
 
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+
 export default function AboutMe() {
-    return(
-        <h1>hi</h1>
-    )
+        return (
+            <Container>
+              <Row>
+                <Col xs={6} md={4}>
+                  <Image src="./images/rosegarden.jpg" thumbnail />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image src="./images/me-conner.jpg" thumbnail />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Image src="./images/beach2.jpg" thumbnail />
+                </Col>
+              </Row>
+              <Row>
+                <h3>About Clare</h3>
+            <p>Hi there! I am Clare, welcome to my portfolio! I am a Chicago native currently enrolled in a Full Stack Bootcamp. Pictured in my photos with me is my amazing boyfriend. We love to run, travel, and hang with friends. </p>
+              </Row>
+            </Container>
+
+         
+          );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,38 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import Project from "../components/Project";
 
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
 export default function Contact() {
     return (
-        <h1> the contact page</h1>
+        <>
+        <Row>
+      
+        <Col xs={6} md={4}>
+        <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon3">
+            https://example.com/users/
+          </InputGroup.Text>
+          <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+        </InputGroup>
+        </Col>
+        <Col xs={6} md={4}>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>$</InputGroup.Text>
+          <Form.Control aria-label="Amount (to the nearest dollar)" />
+          <InputGroup.Text>.00</InputGroup.Text>
+        </InputGroup>
+        </Col>
+        <InputGroup>
+          <InputGroup.Text>With textarea</InputGroup.Text>
+          <Form.Control as="textarea" aria-label="With textarea" />
+        </InputGroup>
+        </Row>
+      </>
     )
 }
